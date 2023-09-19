26f9f2ff8c1671f4d6eff702257c7df6aebdfdca
grucshetskyaleksei <grucshetsky.aleksei@yandex.ru>
2023-09-20 01:24:33 +0300

    Форматирование кода

--- src/fastapi_example/backend/main.py
+++ src/fastapi_example/backend/main.py
@@ -1,9 +1,10 @@
 import logging
 
+import faker
+
 import backend.user.handlers as user_handlers
 import backend.user.repositories as user_repositories
 import backend.user.services as user_services
-import faker
 
 logger = logging.getLogger(__name__)
 
@@ -11,9 +12,7 @@ logger = logging.getLogger(__name__)
 class App:
     def __init__(self):
         self._faker_client = faker.Faker()
-        self._user_faker_client2 = user_repositories.UserFakerClient2(
-            self._faker_client
-        )
+        self._user_faker_client2 = user_repositories.UserFakerClient2(self._faker_client)
         self._user_service = user_services.UserService(self._user_faker_client2)
 
         self._handler1 = user_handlers.UserHandler1(self._user_service)
