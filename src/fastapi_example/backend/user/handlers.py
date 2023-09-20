b44c40f2dab87b7ce932945584b8928a8da9577b
Artem Litvinov <ijaric@gmail.com>
2023-09-20 11:13:20 +0100

    test: bad commit message

--- src/fastapi_example/backend/user/handlers.py
+++ src/fastapi_example/backend/user/handlers.py
@@ -18,3 +18,12 @@ class UserHandler2:
     def get_one(self) -> http_utils.Response:
         user = self._user_service.get_one(entity_id=2)
         return http_utils.get_response_from_dataclass(user)
+
+
+class UserHandler3:
+    def __init__(self, user_service: user_services.UserService):
+        self._user_service = user_service
+
+    def get_one(self) -> http_utils.Response:
+        user = self._user_service.get_one(entity_id=3)
+        return http_utils.get_response_from_dataclass(user)
