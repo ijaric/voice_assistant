fca4d23f059d045e409195c10022a31176e8b133
ksieuk <iebebd3@gmail.com>
2023-09-23 17:52:13 +0300

    [#10] fix move ApiSettings

--- src/fastapi_app/lib/app/split_settings/project.py
+++ src/fastapi_app/lib/app/split_settings/project.py
@@ -4,18 +4,6 @@ import pydantic_settings
 import lib.app.split_settings.utils as app_split_settings_utils
 
 
-class ApiSettings(pydantic_settings.BaseSettings):
-    model_config = pydantic_settings.SettingsConfigDict(
-        env_file=app_split_settings_utils.ENV_PATH,
-        env_prefix="SERVER_",
-        env_file_encoding="utf-8",
-        extra="ignore",
-    )
-
-    host: str = "0.0.0.0"
-    port: int = 8000
-
-
 class ProjectSettings(pydantic_settings.BaseSettings):
     model_config = pydantic_settings.SettingsConfigDict(
         env_file=app_split_settings_utils.ENV_PATH,
