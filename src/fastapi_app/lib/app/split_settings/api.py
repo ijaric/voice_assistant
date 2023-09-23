bde0f0c7e18c7cd6addee9719a85986de0b641e2
ksieuk <iebebd3@gmail.com>
2023-09-23 17:54:26 +0300

    [#10] fix rename SERVER variables to -> API

--- src/fastapi_app/lib/app/split_settings/api.py
+++ src/fastapi_app/lib/app/split_settings/api.py
@@ -6,7 +6,7 @@ import lib.app.split_settings.utils as app_split_settings_utils
 class ApiSettings(pydantic_settings.BaseSettings):
     model_config = pydantic_settings.SettingsConfigDict(
         env_file=app_split_settings_utils.ENV_PATH,
-        env_prefix="SERVER_",
+        env_prefix="API_",
         env_file_encoding="utf-8",
         extra="ignore",
     )
