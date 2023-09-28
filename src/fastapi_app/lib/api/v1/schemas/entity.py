fd84a03d59bcaa56e08f13c710e4af89f19a688f
ksieuk <iebebd3@gmail.com>
2023-09-28 21:28:49 +0300

    add Healthy model

--- src/fastapi_app/lib/api/v1/schemas/entity.py
+++ src/fastapi_app/lib/api/v1/schemas/entity.py
@@ -6,3 +6,7 @@ import pydantic
 class Token(pydantic.BaseModel):
     sub: uuid.UUID
     exp: int | None = None
+
+
+class Healthy(pydantic.BaseModel):
+    status: str = pydantic.Field(..., example="healthy", description="Схема доступности сервиса")
