a7afc8fa7b431fdeee144c355ac80be63b5d41da
Artem Litvinov <ijaric@gmail.com>
2023-10-02 20:08:07 +0100

    build: joke repo & service

--- src/fastapi_app/lib/joke/services.py
+++ src/fastapi_app/lib/joke/services.py
@@ -3,20 +3,27 @@ import logging
 import httpx
 import pydantic
 
-import lib.models.joke as joke_models
+import lib.joke.repository as joke_repository
+import lib.models as models
 
 
 class JokeService:
-    def __init__(self, http_client: httpx.AsyncClient):
-        self.http_client = http_client
+    def __init__(self, repository: joke_repository.JokeRepository):
+        self.repository = repository
         self.logger = logging.getLogger(__name__)
 
-    async def get_joke(self) -> joke_models.Joke | None:
+    async def get_joke(self) -> models.Joke | None:
         try:
-            async with self.http_client as client:
+            async with httpx.AsyncClient() as client:
                 response = await client.get("https://official-joke-api.appspot.com/random_joke")
                 content = response.json()
-                return joke_models.Joke(**content)
+                self.logger.info("Joke retrieved from API")
+                formatted_joke = models.JokeORM(
+                    type_field=content["type"], setup=content["setup"], punchline=content["punchline"]
+                )
+                await self.repository.add_joke(formatted_joke)
+                self.logger.info("Joke added to database")
+                return models.Joke(**content)
         except pydantic.ValidationError as error:
             self.logger.exception("Validation Error: %s", error)
         except httpx.HTTPError as error:
