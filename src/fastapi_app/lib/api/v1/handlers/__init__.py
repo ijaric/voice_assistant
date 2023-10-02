18abf8759874112425c2bf84d81459fee4fcd569
Artem Litvinov <ijaric@gmail.com>
2023-10-02 20:08:31 +0100

    build: joke handler & app

--- src/fastapi_app/lib/api/v1/handlers/__init__.py
+++ src/fastapi_app/lib/api/v1/handlers/__init__.py
@@ -1,6 +1,4 @@
-from .health import *
-from .joke import get_joke, joke_router
+from .health import basic_router
+from .joke import JokeHandler
 
-__all__ = [
-    "basic_router", "joke_router", "get_joke"
-]
+__all__ = ["JokeHandler", "basic_router"]
--- src/fastapi_app/lib/api/v1/handlers/joke.py
+++ src/fastapi_app/lib/api/v1/handlers/joke.py
@@ -1,22 +1,27 @@
 import fastapi
 
 import lib.api.v1.schemas as api_shemas
-import lib.joke.services as services
-
-joke_router = fastapi.APIRouter()
+import lib.joke.services as joke_services
 
 
-@joke_router.get(
-    "/",
-    response_model=api_shemas.JokeResponse,
-    summary="Random joke",
-    description="Return a random joke from a free API.",
-)
-async def get_joke(joke_service: services.JokeService):
-    joke = await joke_service.get_joke()
-    if joke:
-        return api_shemas.JokeResponse(
-            joke=f"{joke.setup}\n{joke.punchline}", id=joke.id_field, category=joke.type_field
+class JokeHandler:
+    router = fastapi.APIRouter()
+
+    def __init__(self, joke_service: joke_services.JokeService):
+        self.joke_service = joke_service
+        self.router = fastapi.APIRouter()
+        self.router.add_api_route(
+            "/",
+            self.get_joke,
+            methods=["GET"],
+            summary="Статус работоспособности",
+            description="Проверяет доступность сервиса FastAPI.",
         )
 
-    return api_shemas.JokeResponse(joke="No joke for you!", id=0, category="No category")
+    async def get_joke(self):
+        joke = await self.joke_service.get_joke()
+        if joke:
+            return api_shemas.JokeResponse(
+                joke=f"{joke.setup}\n{joke.punchline}", id=joke.id_field, category=joke.type_field
+            )
+        return api_shemas.JokeResponse(joke="No joke for you!", id=0, category="No category")
