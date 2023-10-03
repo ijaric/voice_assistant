77118ad539cb8e4c507d63a38bf3fce6caab0491
ksieuk <iebebd3@gmail.com>
2023-10-04 00:57:30 +0300

    fix(temp): [#8] add pyright ignore import error

--- src/template/tests/conftest.py
+++ src/template/tests/conftest.py
@@ -3,7 +3,7 @@ import typing
 
 import fastapi
 import httpx
-import pytest_asyncio  # type: ignore[reportMissingImports]
+import pytest_asyncio  # type: ignore[reportMissingImports, ModuleNotFoundError]
 
 import lib.app as lib_app
 import tests.core.settings as tests_core_settings
