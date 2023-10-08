3c0addcbaca28c32ddb0b8b6b33d984154b43363
ksieuk <iebebd3@gmail.com>
2023-10-08 19:02:35 +0300

    fix(temp): [#8] set default value for password

--- src/template/tests/core/split_settings/postgres.py
+++ src/template/tests/core/split_settings/postgres.py
@@ -17,7 +17,7 @@ class PostgresSettings(pydantic_settings.BaseSettings):
     port: int = 5432
     user: str = "app"
     password: pydantic.SecretStr = pydantic.Field(
-        default=...,
+        default="your_password",
         validation_alias=pydantic.AliasChoices("password", "postgres_password"),
     )
 
